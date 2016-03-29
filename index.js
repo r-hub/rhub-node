74e7788f18c2c726c89f541e56a9ee25e4aaf741
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-29 04:35:39 -0700

    Base and recommended packages

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var optional_dependency_types =  ['Suggests', 'Enhances' ];
 var dependency_types =
     essential_dependency_types.concat(optional_dependency_types);
 
+var base_packages = ['base', 'compiler', 'datasets', 'graphics',
+                     'grDevices', 'grid', 'methods', 'parallel',
+                     'splines', 'stats', 'stats4', 'tcltk', 'tools',
+                     'utils'];
+
+var recommended_packages = ['KernSmooth', 'Matrix', 'mgcv', 'boot',
+			    'class', 'cluster', 'codetools', 'foreign',
+			    'KernSmooth', 'lattice', 'MASS', 'Matrix',
+			    'mgcv', 'nlme', 'nnet', 'rpart', 'spatial',
+			    'survival'];
+
 api = {
     valid_package_name:     '[[:alpha:]][[:alnum:].]*[[:alnum:]]',
     valid_package_version:  '([[:digit:]]+[.-]){1,}[[:digit:]]+',
@@ -12,8 +23,10 @@ api = {
 
     essential_dependency_types: essential_dependency_types,
     optional_dependency_types: optional_dependency_types,
-    dependency_types: dependency_types
+    dependency_types: dependency_types,
 
+    base_packages = base_packages,
+    recommended_packages = recommended_packages
 };
 
 module.exports = api;

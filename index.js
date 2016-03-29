0f1b0f9227b857650d3921e4c023d04cefe7d173
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-29 06:52:53 -0700

    Query CRAN package URLs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var got = require('got');
+var async = require('async');
+var format = require('string-template');
 
 var essential_dependency_types = ['Depends', 'LinkingTo', 'Imports' ];
 var optional_dependency_types =  ['Suggests', 'Enhances' ];
@@ -29,7 +32,62 @@ var cran_src_urls = [
     '{mirror}/src/contrib/Archive/{package}/{package}_{version}.tar.gz'
 ];
 
+var crandb_url = 'https://crandb.r-pkg.org';
+
+function get_latest_cran_version(package, callback) {
+    var url = crandb_url + '/' + package;
+    got(url, function(err, body, response) {
+	if (err) { return callback(err); }
+	callback(null, JSON.parse(body).Version);
+    })
+}
+
+function get_version(package, version, callback) {
+    if (version === undefined) {
+	get_latest_cran_version(package, callback);
+    } else {
+	callback(null, version);
+    }
+}
+
+function check_url(url, callback) {
+    got.head(url, function(err, body, response) {
+	callback(null, !err);
+    });
+}
+
+function get_cran_pkg_url(package, version, callback) {
+    get_version(package, version, function(err, version) {
+	if (err) { return callback(err); }
+	var urls = cran_src_urls.map(
+	    function(x) {
+		return format(x, {
+		    'mirror': cran_mirror,
+		    'package': package,
+		    'version': version
+		})
+	    }
+	);
+	// Return the first one that works
+	async.detectSeries(
+	    urls,
+	    check_url,
+	    function(err, result) {
+		if (err) { return callback(err); }
+		if (result === undefined) {
+		    callback('Cannot find a working URL');
+		} else {
+		    callback(null, result);
+		}
+	    }
+	)
+    })
+}
+
 api = {
+
+    crandb_url: crandb_url,
+
     valid_package_name:     '[[:alpha:]][[:alnum:].]*[[:alnum:]]',
     valid_package_version:  '([[:digit:]]+[.-]){1,}[[:digit:]]+',
     valid_R_system_version: '[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+',
@@ -40,10 +98,13 @@ api = {
     dependency_types: dependency_types,
 
     base_packages: base_packages,
-    recommended_packages: recommended_packages
+    recommended_packages: recommended_packages,
 
     cran_mirror: cran_mirror,
-    cran_src_urls: cran_src_urls
+    cran_src_urls: cran_src_urls,
+
+    get_latest_cran_version: get_latest_cran_version,
+    get_cran_pkg_url: get_cran_pkg_url
 };
 
 module.exports = api;

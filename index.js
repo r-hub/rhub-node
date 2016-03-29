e694a5759a3bc7b88ad5e23021d25a1d53956152
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-29 05:30:37 -0700

    CRAN mirror and URLs

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ var recommended_packages = ['KernSmooth', 'Matrix', 'mgcv', 'boot',
 			    'mgcv', 'nlme', 'nnet', 'rpart', 'spatial',
 			    'survival'];
 
+// Use the string-template package for the templated strings:
+// var format = require("string-template")
+// format(rhub.cran_src_urls[0], {
+//   mirror: rhub.cran_mirror,
+//   package: 'igraph',
+//   version: '1.0.0'
+// })
+
+var cran_mirror = 'https://cran.rstudio.com';
+var cran_src_urls = [
+    '{mirror}/src/contrib/{package}_{version}.tar.gz',
+    '{mirror}/src/contrib/Archive/{package}/{package}_{version}.tar.gz'
+];
+
 api = {
     valid_package_name:     '[[:alpha:]][[:alnum:].]*[[:alnum:]]',
     valid_package_version:  '([[:digit:]]+[.-]){1,}[[:digit:]]+',
@@ -27,6 +41,9 @@ api = {
 
     base_packages: base_packages,
     recommended_packages: recommended_packages
+
+    cran_mirror: cran_mirror,
+    cran_src_urls: cran_src_urls
 };
 
 module.exports = api;

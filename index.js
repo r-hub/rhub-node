0653e3e851365b9f3bf78e0b22172adf2d6bb45b
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-29 04:32:50 -0700

    Add dependency types

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
+var essential_dependency_types = ['Depends', 'LinkingTo', 'Imports' ];
+var optional_dependency_types =  ['Suggests', 'Enhances' ];
+var dependency_types =
+    essential_dependency_types.concat(optional_dependency_types);
+
 api = {
     valid_package_name:     '[[:alpha:]][[:alnum:].]*[[:alnum:]]',
     valid_package_version:  '([[:digit:]]+[.-]){1,}[[:digit:]]+',
     valid_R_system_version: '[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+',
-    valid_numeric_version:  '([[:digit:]]+[.-])*[[:digit:]]+'
+    valid_numeric_version:  '([[:digit:]]+[.-])*[[:digit:]]+',
+
+    essential_dependency_types: essential_dependency_types,
+    optional_dependency_types: optional_dependency_types,
+    dependency_types: dependency_types
+
 };
 
 module.exports = api;
